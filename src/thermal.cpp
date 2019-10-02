64b81b1cda810d9f70060dc11ce6266ab8ce5317
Daniela Rato <danielarato@ua.pt>
2019-10-02 13:48:02 +0100

    converted camera image to ros messages and sucessfully visualized it in rviz

--- src/thermal.cpp
+++ src/thermal.cpp
@@ -9,6 +9,16 @@
 #include <fstream>
 #include <iostream>
 #include <math.h>
+
+/* openCv*/
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/core/core.hpp>
+// #include <opencv2/core/eigen.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+
 //---------Xeneth Camera ------
 #include "XCamera.h"
 
@@ -20,25 +30,41 @@ public:
   // Filter the cloud based on recieved data
   void loop_function();
   XCamera *connectCam();
+  void startCap();
+  void stopCap();
+
+  XCHANDLE handle = 0;
 
 private:
   // rosnode
   ros::NodeHandle nh;
-  // declare functions here
 
+  // declare variables
+  cv::Mat thermal_img;
+  ros::Publisher img_pub;
+  image_transport::ImageTransport it;
+  std_msgs::Header header;
+  sensor_msgs::ImagePtr msg_thermal;
+  ErrCode errorCode = I_OK;
+  dword frameSize = 0; // The size in bytes of the raw image.
+  std::vector<word> framebuffer;
+
+  // declare functions here
   void getImage();
   XDeviceInformation *DeviceDescovery();
 };
 
-ThermalCam::ThermalCam() {}
+ThermalCam::ThermalCam() : it(nh) {
+  img_pub = nh.advertise<sensor_msgs::Image>("thermal_img", 10);
+}
 
 void ThermalCam::loop_function() {
   // DeviceDescovery();
   getImage();
+  img_pub.publish(msg_thermal);
 }
 
 XDeviceInformation *ThermalCam::DeviceDescovery() {
-  ErrCode errorCode = I_OK;
   unsigned int deviceCount = 0;
 
   if ((errorCode = XCD_EnumerateDevices(NULL, &deviceCount, XEF_EnableAll)) !=
@@ -101,16 +127,6 @@ XDeviceInformation *ThermalCam::DeviceDescovery() {
 XCamera *ThermalCam::connectCam() {
 
   XDeviceInformation *dev = DeviceDescovery();
-  // char cDeviceList[1024] = "";
-
-  // Get a list of available devices.
-  // XCamera::GetDeviceList(cDeviceList, sizeof(cDeviceList));
-
-  // ROS_WARN("Device List: %s", cDeviceList);
-
-  // XCameraDlg cameraDlg;
-  // cameraDlg.Create(cDeviceList);
-  // if (cameraDlg.ShowModal() != wxID_OK) return NULL;
 
   XCamera *cam = XCamera::Create(dev->url, NULL, NULL);
 
@@ -127,13 +143,91 @@ XCamera *ThermalCam::connectCam() {
   }
 }
 
-void ThermalCam::getImage() {}
+void ThermalCam::startCap() {
+  XCamera *cam = connectCam();
+  XDeviceInformation *dev = DeviceDescovery();
+  ErrCode errorCode =
+      0; // Used to store returned errorCodes from the SDK functions.
+  handle = XC_OpenCamera(dev->url);
+
+  header.frame_id = "map";
+  header.stamp = ros::Time(0);
+  const char *newint = "64";
+
+  XC_SetPropertyValue(handle, "IntegrationTime", newint, "");
+
+  printf("Start capturing.\n");
+  if ((errorCode = XC_StartCapture(handle)) != I_OK) {
+    printf("Could not start capturing, errorCode: %lu\n", errorCode);
+  } else {
+    printf("Initialization failed\n");
+  }
+}
+
+void ThermalCam::stopCap() {
+  XC_StopCapture(handle);
+  XC_CloseCamera(handle);
+  std::cout << "Camera closed" << std::endl;
+}
+
+// AUTOGAIN
+
+// void XMainFrame::OnAutoGain(wxCommandEvent &evt)
+// {
+//     static FilterID histoFlt = 0;
+//     if(evt.IsChecked())
+//     {
+//         histoFlt = QueueFilter(m_pCam, "AutoGain", "");
+//     }
+//     else
+//     {
+//         m_pCam->RemImageFilter(histoFlt);
+//     }
+// }
+
+void ThermalCam::getImage() {
+
+  // When the connection is initialised, ...
+  if (XC_IsInitialised(handle)) {
+    // ... start capturing
+    if (XC_IsCapturing(handle)) // When the camera is capturing ...
+    {
+
+      // for (;;) {
+
+      // Determine native framesize.
+      frameSize = XC_GetFrameSize(handle);
+
+      // Initialize the 16-bit buffer.
+      // frameBuffer = new word[frameSize / 2];
+      framebuffer.resize(frameSize / 2);
+
+      // ... grab a frame from the camera.
+      printf("Grabbing a frame.\n");
+      if ((errorCode = XC_GetFrame(handle, FT_16_BPP_GRAY, XGF_Blocking,
+                                   framebuffer.data(), frameSize)) != I_OK) {
+        printf("Problem while fetching frame, errorCode %lu", errorCode);
+      }
+
+      int h = XC_GetHeight(handle);
+      int w = XC_GetWidth(handle);
+      // Mat(int rows, int cols, int type, void *data, size_t step =
+      // AUTO_STEP);
+      thermal_img = cv::Mat(h, w,CV_16UC1 , framebuffer.data());
+
+      // std::cout << thermal_img<< std::endl;
+      msg_thermal =
+          cv_bridge::CvImage{header, "mono16", thermal_img}.toImageMsg();
+      // }
+    }
+  }
+}
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "ThermalCam");
   ThermalCam reconstruct;
 
-  reconstruct.connectCam();
+  reconstruct.startCap();
 
   ros::Rate rate(50);
   while (ros::ok()) {
@@ -142,6 +236,7 @@ int main(int argc, char **argv) {
     ros::spinOnce();
     rate.sleep();
   }
+  reconstruct.stopCap();
 
   return 0;
 }
