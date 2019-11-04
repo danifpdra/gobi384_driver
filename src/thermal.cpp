a0db286809b45064cad2bdf95d9d8da9b998273b
Daniela Rato <danielarato@ua.pt>
2019-11-04 13:39:12 +0000

    calibrated camera and published undistorted image

--- src/thermal.cpp
+++ src/thermal.cpp
@@ -18,9 +18,12 @@
 // #include <opencv2/core/eigen.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
+#include <dynamic_reconfigure/server.h>
 #include <ros/package.h>
+#include <thermal_camera/TutorialsConfig.h>
 
 //---------Xeneth Camera ------
 #include "XCamera.h"
@@ -50,24 +53,34 @@ private:
   // declare variables
   XCamera *cam;
   XDeviceInformation *devices, *dev;
-  cv::Mat thermal_img, color_img;
-  image_transport::Publisher img_pub_mono, img_pub_color;
+
+  cv::Mat thermal_img, color_img, undistort_img, camera_matrix, distortion_coef;
+  image_transport::Publisher img_pub_mono, img_pub_color, img_pub_undistort;
   image_transport::ImageTransport it;
   std_msgs::Header header;
-  sensor_msgs::ImagePtr msg_thermal_color, msg_thermal_mono;
+  sensor_msgs::ImagePtr msg_thermal_color, msg_thermal_mono, msg_undistort;
   dword frameSize = 0; // The size in bytes of the raw image.
   std::vector<word> framebuffer;
-  std::string packname;
+  std::string packname, calibration_file;
   FilterID fltThermography = 0; // Handle to the thermography filter.
   std::vector<double> tempLUT;  // Temperature lookup table (ADU to temperature)
   double *tempLUT_debug;
   const char *newint = "64";
   FilterID histoFlt = 0;
 
+  dynamic_reconfigure::Server<thermal_camera::TutorialsConfig> server;
+  dynamic_reconfigure::Server<thermal_camera::TutorialsConfig>::CallbackType f;
+
   // declare functions here
   void getImage();
   XDeviceInformation *DeviceDescovery();
 };
+
+int block, C;
+void callback(thermal_camera::TutorialsConfig &config, uint32_t level) {
+  block = config.block;
+  C = config.C;
+}
 
 /**
  * @brief Construct a new Thermal Cam:: Thermal Cam object
@@ -76,6 +89,7 @@ private:
 ThermalCam::ThermalCam() : it(nh) {
   img_pub_mono = it.advertise("thermal_camera/thermal_img_mono", 1);
   img_pub_color = it.advertise("thermal_camera/thermal_img_color", 1);
+  img_pub_undistort = it.advertise("thermal_camera/undistorted_img", 1);
 }
 
 /**
@@ -83,9 +97,13 @@ ThermalCam::ThermalCam() : it(nh) {
  *
  */
 void ThermalCam::loop_function() {
+
   getImage();
   img_pub_mono.publish(msg_thermal_mono);
   img_pub_color.publish(msg_thermal_color);
+  img_pub_undistort.publish(msg_undistort);
+  f = boost::bind(&callback, _1, _2);
+  server.setCallback(f);
 }
 
 /**
@@ -190,8 +208,22 @@ void ThermalCam::startCap() {
     } else {
       printf("Initialization completed!\n");
     }
-    // }
   }
+  // camera_matrix = Mat_<float>::eye(3, 3);
+  // distortion_coef = Mat_<float>::eye(1, 5);
+
+  std::string calibration_file = path + std::string("/config/ost.yaml");
+  std::cout << calibration_file << std::endl;
+  FileStorage fs(calibration_file, FileStorage::READ);
+  std::cout << "error here" << std::endl;
+  std::cout << fs.isOpened() << std::endl;
+  // fs.open(calibration_file, FileStorage::READ);
+  fs["camera_matrix"] >> camera_matrix;
+  fs["distortion_coefficients"] >> distortion_coef;
+  fs.release();
+
+  std::cout << camera_matrix << std::endl;
+  std::cout << distortion_coef << std::endl;
 }
 
 /**
@@ -203,33 +235,33 @@ void ThermalCam::getImage() {
   // When the connection is initialised, ...
   if (XC_IsInitialised(handle)) {
     // ... start capturing
-    if (XC_IsCapturing(handle)) // When the camera is capturing ...
+    // if (XC_IsCapturing(handle)) // When the camera is capturing ...
     {
       // std::cout << "debugging 1" << std::endl;
-      fltThermography = XC_FLT_Queue(handle, "Thermography", "celsius");
-      // histoFlt = XC_FLT_Queue(handle, "AutoGain", "");
-      if (fltThermography > 0) {
+      // fltThermography = XC_FLT_Queue(handle, "Thermography", "celsius");
+      // // histoFlt = XC_FLT_Queue(handle, "AutoGain", "");
+      // if (fltThermography > 0) {
 
-        // Build the look-up table and ..
-        dword mv = XC_GetMaxValue(handle);
+      //   // Build the look-up table and ..
+      //   dword mv = XC_GetMaxValue(handle);
 
-        tempLUT_debug = new double[mv + 1];
+      //   tempLUT_debug = new double[mv + 1];
 
-        tempLUT.resize(mv + 1);
+      //   tempLUT.resize(mv + 1);
 
-        for (dword x = 0; x < mv + 1; x++) {
+      //   for (dword x = 0; x < mv + 1; x++) {
 
-          double *temp;
-          XC_FLT_ADUToTemperature(handle, fltThermography, x,
-                                  &tempLUT_debug[x]);
-          tempLUT[x] = tempLUT_debug[x];
-        }
+      //     double *temp;
+      //     XC_FLT_ADUToTemperature(handle, fltThermography, x,
+      //                             &tempLUT_debug[x]);
+      //     tempLUT[x] = tempLUT_debug[x];
+      //   }
 
-        // for (double n : tempLUT) {
-        //   std::cout << n << ",";
-        // }
-        // std::cout << "\n" << std::endl;
-      }
+      // for (double n : tempLUT) {
+      //   std::cout << n << ",";
+      // }
+      // std::cout << "\n" << std::endl;
+      // }
 
       /***/
 
@@ -255,9 +287,6 @@ void ThermalCam::getImage() {
 
       cv::Mat color_img = cv::Mat(h, w, CV_16UC1, framebuffer.data());
 
-      // color_img *= 4;
-      // color_img /= 128; // convert pixel values to 8bit
-
       color_img.convertTo(color_img, CV_8UC1,
                           1 / 256.0); // convert image to 8bit
 
@@ -267,6 +296,15 @@ void ThermalCam::getImage() {
       //                     0); // increase the contrast by 4
 
       color_img = cv::Scalar::all(255) - color_img;
+      thermal_img = cv::Scalar::all(256 * 256) - thermal_img;
+      // cv::threshold(color_img, color_img, 0, 255,
+      //               cv::THRESH_BINARY + cv::THRESH_OTSU);
+
+      cv::threshold(color_img, color_img, block, 255, cv::THRESH_BINARY);
+
+      // cv::adaptiveThreshold(color_img, color_img, 255,
+      //                       cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY,
+      //                       block, C);
 
       // double inverse_gamma = 1.0 / 0.1;
 
@@ -296,6 +334,10 @@ void ThermalCam::getImage() {
         COLORMAP_SUMMER
         COLORMAP_WINTER*/
       // cv::applyColorMap(color_img, color_img, cv::COLORMAP_AUTUMN);
+      cv::undistort(thermal_img, undistort_img, camera_matrix, distortion_coef);
+
+      msg_undistort = cv_bridge::CvImage{header, "mono16", undistort_img}
+                          .toImageMsg(); /*convert image to ROS msg*/
 
       msg_thermal_mono = cv_bridge::CvImage{header, "mono16", thermal_img}
                              .toImageMsg(); /*convert image to ROS msg*/
